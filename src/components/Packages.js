3e480e6104ef0e4a6ff2aee0807a291460183f47
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-06-18 12:40:09 -0400

    first commit, implemented the basics for the in class activity in module 7

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,67 @@
-
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage) => 
+        <ListGroup.Item>{eachPackage}</ListGroup.Item>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-            </div>
-        </div>
+    <div>  
+          {/* I'm curious on how Carousel is implemented. Will work with Carousel later.  */}
+        {/* <Carousel>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel> */}
+
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Our Packages</Card.Title>
+                    <Card.Text>
+                        Check out some of our packages! Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
+                    {displayPackages} 
+                </ListGroup>
+            </Card>
+        </Container>
+    </div>    
+
     )
 }
